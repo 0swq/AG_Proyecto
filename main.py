ce8f6d7c524461a2074f2d4a42bd5bba7279c738
cpomibnados <u23246529@utp.edup.pe>
2025-12-09 00:54:35 -0500

    commit

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ ProductosController.crear("Pollo - 1/2","plato_principal",15,10,5)
 ProductosController.crear("Pollo - 1","plato_principal",40,30,5)
 ProductosController.crear("Pollo - 1/8","plato_principal",8,4,5)
 
-
+#push
 empleado_controller = EmpleadoController()
 Global.usuario_actual= empleado_controller.crear(nombre="aw", rol="administrador", usuario="user", password="pass123")
 
